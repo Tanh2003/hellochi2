a7dc2d55b861280be5b64e4005143705c73422cf
Tanh2003 <nhoxtuananh092@gmail.com>
2023-10-21 22:18:47 +0700

    lan2

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bé Chang của anh ơi',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'bấm vào đây đi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'yêu anh nhất<33',
+    btnNo: 'Không,yêu anh đâu:3',
+    question:'Trên thế giới hơn 7 tỉ người  thì anh yêu em nhất á <3.Em có gì muốn nói với anh hem',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://tanh2003.github.io/hello_ch-/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
